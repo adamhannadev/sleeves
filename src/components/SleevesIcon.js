35cc229aa80ca7d2aa5c07af542b5dacbb63df62
adamhanna <adamhannadev@gmail.com>
2020-06-16 14:07:56 -0700

    Fetch weather API and output current temperature

--- src/components/SleevesIcon.js
+++ src/components/SleevesIcon.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SleevesIcon = () => {
-    return (
-        <div className="sleeves-icon">
-            <h2>Sleeves Icon</h2>
-        </div>
-    );
+class SleevesIcon extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: null,
+        };
+    }
+
+    componentDidMount() {
+        const access_key = 'dadd8a499b170b2dbeed3cceb3121419';
+        const query = 'Victoria, Canada';
+        fetch(`http://api.weatherstack.com/current?access_key=${access_key}&query=${query}`)
+            .then(response => response.json())
+            .then(data => this.setState({ data }));
+    }
+
+    render(props) {
+        const { data } = this.state;
+        return (
+            <div className="sleeves-icon" >
+                <h2>Sleeves Icon</h2>
+                {data ? data.current.temperature : 'None'}
+            </div>
+        );
+    }
 }
 
 export default SleevesIcon;

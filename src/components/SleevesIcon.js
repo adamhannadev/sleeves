d42e0dcd3a23854425d79897b578adaddfecb2d0
adamhanna <adamhannadev@gmail.com>
2020-06-16 13:21:56 -0700

    Create icon component

---
+++ src/components/SleevesIcon.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SleevesIcon = () => {
+    return (
+        <div className="sleeves-icon">
+            <h2>Sleeves Icon</h2>
+        </div>
+    );
+}
+
+export default SleevesIcon;
